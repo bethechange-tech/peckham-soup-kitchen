af760f9beaeb005ace89541712650736dad7b4d5
personal <personal@personals-MacBook-Pro.local>
2024-07-31 10:26:55 +0100

    updated meadata

--- src/app/(app)/(unprotected)/layout.tsx
+++ src/app/(app)/(unprotected)/layout.tsx
@@ -1,21 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import type { Metadata, Viewport } from "next";
 
-// Define metadata for the Peckham Soup Kitchen project
-export const metadata: Metadata = {
-  title: "Peckham Soup Kitchen - Community Support",
-  description: "Providing essential services and support to the Southwark community through food provision and community programs.",
-};
-
-
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  interactiveWidget: 'resizes-visual',
-}
 
 export default function RootLayout({
   children,
